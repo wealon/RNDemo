4439ad74575df7a55ca392aaad142e8b6bf9bcf9
Datou <liuzhixiang@91sph.com>
2020-08-13 22:37:05 +0800

    Initial Commit

---
+++ RNDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RNDemo
+//
+//  Created by wealon on 2020/8/13.
+//  Copyright © 2020 wealon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
